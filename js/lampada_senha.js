58b4084f27d5a181236eb10a279d129d89ef50a5
unknown <laurasc1603@gmail.com>
2024-05-30 00:22:11 -0300

    lampada_s

--- js/lampada_senha.js
+++ js/lampada_senha.js
@@ -8,6 +8,7 @@ function interruptor(){
             lamp.src = 'img/lampada_acesa.png'
             document.querySelector("#h3").innerHTML = "Lampada acesa! Digite a senha para apagar novamente"
             document.querySelector("#btn").innerHTML = "INTERRUPTOR"
+
             document.body.style.backgroundColor = "#FFFF7EF3"
             document.querySelector("#h3").style.color = "black"
             document.body.style.color = "black"
@@ -16,18 +17,21 @@ function interruptor(){
             lamp.src = 'img/lampada_apagada.png'
             document.querySelector("#h3").innerHTML = "Digite a senha para acender a lampada"
             document.querySelector("#btn").innerHTML = "INTERRUPTOR"
+
             document.body.style.backgroundColor = "black"
             document.querySelector("#h3").style.color = "white"
             document.body.style.color = "white"
         }
     }
-    if ((senha != "info2024") && (contador < 3)){
+    if (senha != "info2024"){
         while (contador < 3){
             contador++
+
             if (contador < 3){
                 lamp.src = 'img/lampada_apagada.png'
                 document.querySelector("#h3").innerHTML = "Digite a senha para acender a lampada"
                 document.querySelector("#btn").innerHTML = "INTERRUPTOR"
+
                 document.body.style.backgroundColor = "black"
                 document.querySelector("#h3").style.color = "white"
                 document.body.style.color = "white"
@@ -37,6 +41,7 @@ function interruptor(){
                 lamp.src = 'img/lampada_quebrada.png'
                 document.querySelector("#h3").innerHTML = "Digite a senha correta para consertar a lampada."
                 document.querySelector("#btn").innerHTML = "CONSERTAR"
+
                 document.body.style.backgroundColor = "black"
                 document.querySelector("#h3").style.color = "white"
                 document.body.style.color = "white"
